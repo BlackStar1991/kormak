980225ee6797117efd15c973f105c26deae4c587
BlackStar1991 <zirkaandry@ukr.net>
2017-07-30 14:55:33 +0300

    responsive view

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
 
+//MENU
+    var headerPage = $('.header');
+
+    $('.open-menu-min').on("click", function () {
+        $(this).toggleClass('close-menu-min');
+        $(".bl_nav__mobile").toggleClass('closeMenu');
+
+    });
+
+//     headerPage.on('click', '.open-menu-main', function () {
+//         headerPage.removeClass('open-sub-menu');
+//         headerPage.toggleClass('open-main-menu');
+//     });
+//
+// //search
+//     headerPage.on('click', '.mobile-ico__search', function () {
+//         headerPage.removeClass('open-sub-menu');
+//         headerPage.toggleClass('open-search');
+//         headerPage.removeClass('open-main-menu');
+//     });
+//
+//     headerPage.on('click', '.close-search', function () {
+//         headerPage.toggleClass('open-search');
+//     });
+//
+// //footer
+//     $('.footer-page').on('click', '.h3-title', function () {
+//         $(this).closest('.foot-info-list__title').toggleClass('active');
+//     });
+
+
+//////////////////////////////////////////
 
 //   sliderEffect
     function sliderEffectForButtons(button, element, duration) {
@@ -18,9 +50,9 @@ $(document).ready(function () {
     }
 
 
-   var buttonLanguage = $(".bl_language__active"),
-       fieldWithOtherLanguage =$(".bl_language__variables"),
-       buttonChooseRigthLanguage = $(".bl_language__variables_item");
+    var buttonLanguage = $(".bl_language__active"),
+        fieldWithOtherLanguage = $(".bl_language__variables"),
+        buttonChooseRigthLanguage = $(".bl_language__variables_item");
 
     hideShowEffect(buttonLanguage, fieldWithOtherLanguage);
     hideShowEffect(buttonChooseRigthLanguage, fieldWithOtherLanguage);
@@ -30,8 +62,7 @@ $(document).ready(function () {
     }
 
 
-
-    //// Main-slider
+ //// Main-slider
 
     $(".bl_mainSlider").owlCarousel({
         items: 1,
@@ -47,7 +78,7 @@ $(document).ready(function () {
         pagination: false,
         responsiveClass: true,
         responsive: {
-            600: {
+            320: {
                 items: 1
             }
         }
@@ -69,15 +100,25 @@ $(document).ready(function () {
         pagination: false,
         responsiveClass: true,
         responsive: {
-            // 600: {
-            //     items: 4
-            // }
+            1200: {
+                items: 4
+            },
+            992: {
+                items: 3
+            },
+
+            550: {
+                items: 2
+            },
+            320: {
+                items: 1
+            }
+
         }
     });
 
 
-
-    // slider_newProducts
+////// slider_newProducts
 
     $(".slider_newProducts").owlCarousel({
         items: 4,
@@ -93,12 +134,48 @@ $(document).ready(function () {
         pagination: false,
         responsiveClass: true,
         responsive: {
-            // 600: {
-            //     items: 4
-            // }
+            1200: {
+                items: 4
+            },
+            992: {
+                items: 3
+            },
+            550: {
+                items: 2
+            },
+            320: {
+                items: 1
+            }
+
         }
     });
 
+////hover sliders srrows navigations
+
+    function hoverArrows(sliderBox, arrowPrev, arrowNext, changedClass) {
+
+        sliderBox.hover(function () {
+                arrowPrev.addClass(changedClass);
+                arrowNext.addClass(changedClass);
+            },
+            function () {
+                arrowPrev.removeClass(changedClass);
+                arrowNext.removeClass(changedClass);
+            });
+    }
+
+var blockPopular = $(".bl_popular"),
+    arrowPrev_popular = $(".slider_popular.owl-carousel .owl-prev"),
+    arrowNext_popular = $(".slider_popular.owl-carousel .owl-next"),
+
+    blockNewProducts = $(".bl_newProducts"),
+    arrowPrev_newProducts = $(".slider_newProducts.owl-carousel .owl-prev"),
+    arrowNext_newProducts = $(".slider_newProducts.owl-carousel .owl-next"),
+
+    hover_arrowsClass = "arrow-prev__hover";
+
+    hoverArrows(blockPopular, arrowPrev_popular, arrowNext_popular, hover_arrowsClass);
+    hoverArrows(blockNewProducts, arrowPrev_newProducts, arrowNext_newProducts, hover_arrowsClass);
 
 
 // Ancor to top
@@ -115,53 +192,61 @@ $(document).ready(function () {
 
 // Showed ancor only when scrolling > scrineSize
 
-// $(window).scroll(function() {
-//
-//     var topNow = blockNavigation.offset().top;
-//     var leftNow = blockNavigation.offset().left;
-//
-//     if ( ($(this).scrollTop() > startFixed) && (screenWidth >= 1200) ) {
-//
-//
-//
-//
-//         addFluid.removeClass("hidden");
-//
-//
-//         addSomeClass(blockNavigation, "my_fixed");
-//         addSomeClass(blockRepresent , "my_fixed__represent");
-//         addSomeClass(blockTelephones, "my_fixed__telephones");
-//         addSomeClass(blockCallback, "my_fixed__callback");
-//
-//         takeRightTop(addFluid, topNow, -5);
-//
-//
-//         takeRightTop(blockRepresent, topNow, 5, leftNow, 310);
-//         takeRightTop(blockTelephones, topNow, -4, leftNow, 540);
-//         takeRightTop(blockCallback, topNow, -2, leftNow, 920 );
-//
-//
-//
-//     } else if($(this).scrollTop() <= startFixed ) {
-//
-//
-//         addFluid.addClass("hidden");
-//
-//
-//
-//
-//         removeSomeClass(blockNavigation, "my_fixed");
-//         removeSomeClass(blockRepresent, "my_fixed__represent");
-//         removeSomeClass(blockTelephones, "my_fixed__telephones");
-//         removeSomeClass(blockCallback, "my_fixed__callback");
-//
-//
-//
-//         topZiro($(".bl_represent"));
-//         topZiro($(".bl_telephones"));
-//         topZiro($(".bl_callback"));
-//
-//
-//     }
-//
-// });
+$(window).scroll(function() {
+
+    // var topNow = blockNavigation.offset().top;
+    // var leftNow = blockNavigation.offset().left;
+    //
+    // if ( ($(this).scrollTop() > startFixed) && (screenWidth >= 1200) ) {
+    //
+    //
+    //
+    // } else if($(this).scrollTop() <= startFixed ) {
+    //
+    //
+    // }
+
+});
+
+
+
+
+
+// RESIZE WINDOW
+
+var body = $("body");
+
+var iconMenu = $(".open-menu-min"),
+    iconMenu_close = "close-menu-min";
+
+var fullNavigation = $(".bl_fullNavigation"),
+    mobileNavigation = "bl_fullNavigation_mobile",
+
+    blockNavigation = $(".bl_nav"),
+    menuMobile = "bl_nav__mobile",
+    closeMenu = "closeMenu";
+
+
+
+$(window).resize(function(){
+    if( body.width() >= 1200 ){
+
+        fullNavigation.removeClass(mobileNavigation);
+
+        blockNavigation.removeClass(menuMobile);
+        blockNavigation.removeClass(closeMenu);
+    }else {
+        fullNavigation.addClass(mobileNavigation);
+
+
+        blockNavigation.addClass(menuMobile);
+        blockNavigation.addClass(closeMenu);
+
+        iconMenu.removeClass(iconMenu_close);
+    }
+
+
+
+
+});
+$(window).resize();
