405ef42eb8fcfab116153bb781e4e32d8eebba20
DELL-PC\Andry Zirka <zirkaandry@gmail.com>
2017-09-22 18:24:27 +0300

    corrected menu

--- dist/js/common.min.js
+++ dist/js/common.min.js
@@ -573,6 +573,8 @@ $(document).ready(function () {
                 blackWrapper.addClass("hidden");
                 blSuccsess.addClass("hidden");
             },1000);
+
+
         }
 
 
@@ -600,11 +602,45 @@ $(document).ready(function () {
 
         if (quantityProduct.val() > 1) {
             quantityProduct.val(Number(quantityProduct.val()) - 1);
+            quantityProduct.val(Number(quantityProduct.val()) - 1);
         }
         else {
             quantityProduct.val(1);
         }
     });
+
+
+
+// Road Map
+
+    function roadMap() {
+        var blockMap = $(".bl_map"),
+            buttonCloseMap = $(".bl_map__close");
+
+        $(".button_map").on("click", function () {
+            blackWrapper.removeClass("hidden");
+            blackWrapper.animate({opacity: 0.6}, 300);
+            blockMap.removeClass("hidden");
+
+            if ( $(".bl_map__iframe").length == 0){
+                $('<iframe class="bl_map__iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.2340071664594!2d30.62924531598441!3d50.455366979476075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c554bbd79077%3A0x433aacf838798f5b!2z0JrQntCg0JzQkNCa!5e0!3m2!1sru!2sru!4v1496344340189" frameborder="0" style="border:0"></iframe>').insertAfter(".bl_map__title");
+            }
+        });
+
+        buttonCloseMap.on("click", function () {
+            blackWrapper.animate({opacity: 0}, 200);
+            blackWrapper.addClass("hidden");
+            blockMap.addClass("hidden");
+        });
+
+        blackWrapper.on("click", function () {
+            blockMap.addClass("hidden");
+        });
+
+    }roadMap();
+
+    
+    
 
 
 });
